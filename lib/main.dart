1420ccefa0f6e0fbfe8e5720e388cd6f1cceeeb6
Aftab Uddin Alif <mdaftabuddinalif@gmail.com>
2023-08-08 11:12:51 +0600

    adding product model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:food_app_getx/pages/food/recommended_food_detail.dart';
 import 'package:get/get.dart';
+import 'helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
