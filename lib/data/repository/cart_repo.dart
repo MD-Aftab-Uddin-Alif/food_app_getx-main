f2511235c72a5cfa2d9cef3b17a40adb59e8ba29
Aftab Uddin Alif <mdaftabuddinalif@gmail.com>
2023-08-13 12:49:20 +0600

    adding shared preferences and store cart data dynamically

--- lib/data/repository/cart_repo.dart
+++ lib/data/repository/cart_repo.dart
@@ -1,1 +1,36 @@
-class CartRepo {}
+import 'dart:convert';
+
+import 'package:food_app_getx/models/cart_model.dart';
+import 'package:food_app_getx/util/app_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartRepo {
+  final SharedPreferences sharedPreferences;
+  CartRepo({required this.sharedPreferences});
+
+  List<String> cart = [];
+  void addToCartList(List<CartModel> cartList) {
+    cart = [];
+    /* ------------------------ convert objects to string ----------------------- */
+
+    for (var element in cartList) {
+      cart.add(jsonEncode(element));
+    }
+
+    sharedPreferences.setStringList(AppConstants.CART_LIST, cart);
+    getCartList();
+  }
+
+  List<CartModel> getCartList() {
+    List<String> carts = [];
+    if (sharedPreferences.containsKey(AppConstants.CART_LIST)) {
+      carts = sharedPreferences.getStringList(AppConstants.CART_LIST)!;
+    }
+    List<CartModel> cartList = [];
+
+    for (var element in carts) {
+      cartList.add(CartModel.fromJson(jsonDecode(element)));
+    }
+    return cartList;
+  }
+}
